c4204efa138ecaa659f84bb75cf9d031be983a10
Volodymyr <vovakrivickiy34@gmail.com>
2019-06-14 21:58:37 +0200

    Updates

---
+++ precache-manifest.3fef997d9c34f990b7cc778aeb79e34a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff398cb3755e472470a726ba284b4fb",
+    "url": "/todo_app_react/index.html"
+  },
+  {
+    "revision": "bb8ae5904f5713d9af33",
+    "url": "/todo_app_react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/todo_app_react/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bb8ae5904f5713d9af33",
+    "url": "/todo_app_react/static/js/main.a9d7a70a.chunk.js"
+  },
+  {
+    "revision": "e6fc570d17e1e74575a6",
+    "url": "/todo_app_react/static/js/runtime~main.00441f27.js"
+  }
+]);
